b784a689b6aa18a466067b623a253e8ca6d06fbf
ro-drigo <rodrigo.santos7202@gmail.com>
2020-05-01 17:31:47 -0300

    Adding login page frontend

---
+++ frontend/src/Components/Input/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Input = styled.TextInput`
+width: 370px;
+height: 50px;
+color: #fff;
+text-align: center;
+margin: 20px;
+background: #51B2BC;
+border-radius: 30px;
+
+`;
--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Input } from './../../Components/Input'
+import { Container, Title, LoginButton } from './styles';
+import { render } from 'react-dom';
 
-// import { Container } from './styles';
 
 export default function Login() {
+  
   const navigation = useNavigation();
 
   function navigateToRegister() {
@@ -16,9 +19,36 @@ export default function Login() {
   }
 
   return (
-    <View>
-         <Text onPress={navigateToRegister}>Register</Text>
-         <Text onPress={navigateToHome}>Home</Text>
-    </View>
+    <Container>
+
+      <Image 
+        style={{width: 250, bottom: 50}}
+        source={require('./../../../assets/logotipo.png')}
+      />
+
+      <Text style={{bottom: 100, right: 50, color: '#4F4F4F' }}>Faça seu login e entre </Text>
+      <Text style={{bottom: 100, right: 50, color: '#4F4F4F' }}>nessa economia!</Text>
+
+      <View>
+          <Title style={{left: 35, top: 10}}>Usuário</Title>
+          <Input />
+
+          <Title style={{left: 35, top: 10}}>Senha</Title>
+          <Input />
+      </View>
+
+      <View>
+
+        <Text style={{textAlign: "center", top: 55, color: "#4F4F4F"}}>Esqueci a senha</Text>
+        
+        <LoginButton onPress={navigateToHome}><Text style={{textAlign: "center", color: "white", fontSize: 20, top: 10}}>Entrar</Text></LoginButton>
+
+        <Text onPress={navigateToRegister} style={{textAlign: "center", top: 100, color: "#4F4F4F"}}>Não possui um cadastro?</Text>
+
+      </View>
+      
+    </Container>
   );
+  
 }
+
