6f8f37261178c143764e72121430f08e7559d013
ro-drigo <rodrigo.santos7202@gmail.com>
2020-05-02 17:26:10 -0300

    Adding Register rent, register goals and profile pages

---
+++ frontend/src/Pages/Profile/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+     flex: 1;
+     justify-content: center;
+     align-items: center;
+`;
+
+
+export const Title = styled.Text`
+    font-size: 30px;
+    color: #4F4F4F;
+    position: relative;
+    margin-left: 20%;
+    width: 200px;
+`;
+
+export const SubTitle = styled.Text`
+    font-size: 20px;
+    color: #BEBEBE;
+    position: relative;
+    margin-left: 10%;
+    width: 200px;
+    margin-top: 3%;
+    margin-bottom: 10%;
+`;
+
+export const FieldTitle = styled.Text`
+    font-size: 20px;
+    color: black;
+    position: relative;
+
+`;
+
+
+
+export const ProfileBox = styled.View`
+    border-radius: 30px;
+    width: 90%;
+    height: 80px;
+    background: #51B2BC;
+    margin-bottom: 5%;
+`;
--- frontend/src/Pages/RegisterGoals/index.js
+++ frontend/src/Pages/RegisterGoals/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, Picker } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Container, Title, Viewer, FinishButton, SaveAddButton, PickerBlinders, ListGoals } from './styles';
+import { Input } from './../../Components/Input';
 
 // import { Container } from './styles';
 
 export default function RegisterGoals() {
+
+  const [selectedValue, setSelectedValue] = useState("Escolha");
+
   const navigation = useNavigation();
 
   function navigateToHome() {
@@ -12,8 +17,46 @@ export default function RegisterGoals() {
   }
   
   return (
-     <View>
-          <Text onPress={navigateToHome}>Vai</Text>
-     </View>
+    <Container>
+
+    <View>
+      <Image 
+        style={{width: 250}}
+        source={require('./../../../assets/logotipo.png')}
+      />
+
+      <Text style={{paddingBottom: 20, bottom: 50, color: '#4F4F4F', width: 150 }}>Registre suas metas...</Text>
+    </View>
+
+    <Viewer>
+      <Title style={{width: 500}}>Nome da meta:</Title>
+      <Input/>
+
+      <Title style={{width: 500}}>Valor:</Title>
+      <Input/>
+
+      <Title style={{width: 500}}>Proporção:</Title>
+
+
+    </Viewer>
+
+  <ListGoals>
+    <PickerBlinders
+        selectedValue={selectedValue}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
+
+        <Picker.Item label="Escolha" value="" />
+        <Picker.Item label="Metinha" value="Metinha" />
+        <Picker.Item label="Meta" value="Meta" />
+        <Picker.Item label="Metona" value="Metona" />
+        
+      </PickerBlinders>
+    </ListGoals>
+
+      
+         <SaveAddButton><Text style={{textAlign: "center", color: "white", fontSize: 20, top: 10, letterSpacing: 1.2}}>SALVAR E ADICIONAR OUTRA</Text></SaveAddButton>
+         <FinishButton onPress={navigateToHome}><Text style={{textAlign: "center", color: "white", fontSize: 20, top: 10, letterSpacing: 1.2}}>FINALIZAR</Text></FinishButton>
+      
+     </Container>
   );
 }
