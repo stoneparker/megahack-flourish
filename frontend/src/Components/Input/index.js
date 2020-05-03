0df449d82b373cd7766cd9598b25ae475d3932bd
Vinicius Floriano <viniciusfloriano05@gmail.com>
2020-05-03 15:58:40 -0300

    Updated Login, Register, Register Rent

--- frontend/src/Components/Input/index.js
+++ frontend/src/Components/Input/index.js
@@ -13,26 +13,20 @@ export const Input = styled.TextInput`
 `;
 
 export const DataInput = styled.TextInput`
-    width: 45%;
+    width: 100%;
     height: 45px;
     color: #fff;
     text-align: center;
-    margin-bottom: 5%;
     background: #51B2BC;
     border-radius: 30px;
-    margin-right: 38%;
 `;
 
 export const GenInput = styled.TextInput`
-    width: 8%;
+    width: 100%;
     height: 45px;
     color: #fff;
     text-align: center;
-    margin-bottom: 5%;
     background: #51B2BC;
     border-radius: 30px;
-    margin-right: 30%;
-    position: absolute;
-    left: 68%;
 `;
 
--- frontend/src/Pages/Register/index.js
+++ frontend/src/Pages/Register/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { ScrollView, Text, Image } from 'react-native';
+import { ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-//import { AppLoading } from 'expo';
-//import { useFonts } from '@use-expo/font';
-import { Input, DataInput, GenInput } from './../../Components/Input';
 
-import { Container, Title, Box, GenTitle, NextButton } from './styles';
+import { Input, DataInput, GenInput } from './../../Components/Input';
+import { Container, Title } from '../../Utils/Styles/GlobalStyles';
+import Header from '../../Components/Header';
+import Button from '../../Components/Button';
+
+import { Box, GenTitle, FirstBoxDivision, SecondBoxDivision } from './styles';
 
 export default function Register() {
   const navigation = useNavigation();
@@ -20,34 +22,36 @@ export default function Register() {
   
   return (
   <ScrollView>
-  <Container>
-    <Image 
-        style={{width: 250, marginTop: 15}}
-        source={require('./../../../assets/logotipo.png')}
-      />
-      <Text style={{bottom: 50, color: '#4F4F4F', width: 200 }}>Complete com suas informações...</Text>
+    <Container>
+        <Header title="Complete com suas informações..." />
 
 
-      <Title>Usuário</Title>
-      <Input />
+        <Title>Usuário</Title>
+        <Input />
 
-      <Title>Email</Title>
-      <Input />
+        <Title>Email</Title>
+        <Input />
 
-      <Title>Senha</Title>
-      <Input />
+        <Title>Senha</Title>
+        <Input />
 
-      <Title>Confirmar Senha</Title>
-      <Input />
+        <Title>Confirmar Senha</Title>
+        <Input />
 
-    <Box>
-      <Title>Data de Nascimento</Title>
-      <DataInput />
-      <GenTitle>Gênero</GenTitle>
-      <GenInput />
-    </Box>
-    <NextButton onPress={navigateToRegisterRent}><Text style={{textAlign: "center", color: "white", fontSize: 20, top: 10, letterSpacing: 1.2}}>PRÓXIMO</Text></NextButton>
-  </Container>
+      <Box>
+        <FirstBoxDivision>
+          <Title>Data de Nascimento</Title>
+          <DataInput />
+        </FirstBoxDivision>
+        <SecondBoxDivision>
+          <GenTitle>Gênero</GenTitle>
+          <GenInput />
+        </SecondBoxDivision>
+      </Box>
+
+      <Button onPress={navigateToRegisterRent} text="PRÓXIMO" />
+      
+    </Container>
   </ScrollView>
   );
 }
